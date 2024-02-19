cd657b94e81f118e8cada13f5c383bf3d32561f5
Your <hcagataybayrak@gmail.com>
2024-02-19 15:25:12 +0300

    aaaa u

--- stat295.R
+++ stat295.R
@@ -2,3 +2,15 @@
 
 1+1
 
+install.packages( c("devtools", "roxygen2", "testthat", "rmarkdown", "pkgdown", "purr"))
+
+devtools::has_devel()
+
+usethis::use_git_config(
+  user.name = "dattik12"
+  user.email = "hcagataybayrak@gmail.com"
+)
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
